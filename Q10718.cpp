7d817680554328693c6fda87ce374e0bf2fd5c97
Min-gi Jeong <jmk1118@naver.com>
2020-02-08 19:53:06 +0900

    10171 추가

--- Q10718.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main(void)
-{
-	std::cout << "강한친구 대한육군" << std::endl;
-	std::cout << "강한친구 대한육군" << std::endl;
-
-	return 0;
-}
---
+++ 번호별/Q10171.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(void)
+{
+	std::cout << "\\    /\\" << std::endl;
+	std::cout << " )  ( ')" << std::endl;
+	std::cout << "(  /  )" << std::endl;
+	std::cout << " \\(__)|";
+}

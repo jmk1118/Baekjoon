5656ad914a9ea0b33ad7920460092c3a213470c6
jmk1118 <jmk1118@naver.com>
2021-07-10 16:12:56 +0900

    add 14889

--- Baekjoon/소스.cpp
+++ Baekjoon/소스.cpp
@@ -1,90 +1,119 @@
 #include <iostream>
 #include <algorithm>
 
-#define PLUS 1
-#define MINUS 2
-#define MULTI 3
-#define DIV 4
+int N; // 팀의 수
+int S[21][21]; // 입력받은 능력치
+int min; // 1팀과 2팀의 능력치 차이 최솟값
+bool ateam[21]; // 1팀에 들어간 선수는 true가 된다.
 
-int N; // 수열 수
-int signNumber[5]; //1 : 덧셈, 2 : 뺄셈, 3 : 곱셈, 4 : 나눗셈
-int number[12]; // 입력받은 수열
-int sign[12]; // 백트래킹 함수에서 현재 부호들 상태를 저장할 배열, 덧셈 : 1, 뺄셈 : 2, 곱셈 : 3, 나눗셈 : 4
-int max, min; // 답으로 제출할 최댓값, 최솟값
-
-void EnterSign(int location); // 부호를 백트래킹하며 집어넣는 함수
-int Calculation(); // 부호를 모두 채워넣었을 때 계산해줄 함수
+void AddPerson(int nextPerson, int size);
+int CalculateStats();
 
 int main(void)
 {
-	// 입력값 입력
-	std::cin >> N;
+	std::cin >> N; // N 입력
+	
+	// S 입력
 	for (int i = 1; i <= N; i++)
-		std::cin >> number[i];
-	std::cin >> signNumber[1] >> signNumber[2] >> signNumber[3] >> signNumber[4];
-
-	max = -1000000001;
-	min = 1000000001;
-
-	// 첫 칸부터 백트래킹
-	sign[1] = PLUS; // 첫 숫자는 +나 마찬가지이므로 +로 초기화
-	EnterSign(2);
-
-	std::cout << max << std::endl;
-	std::cout << min << std::endl;
-}
-
-void EnterSign(int location) // 부호를 백트래킹하며 집어넣는 함수
-{
-	for (int i = 1; i <= 4; i++)
 	{
-		// 덧셈, 뺼셈, 곱셈, 나눗셈 순서로 집어넣어본다. 단, 해당 부호가 남아있지 않다면 패스한다.
-		if (signNumber[i] <= 0)
-			continue;
-
-		signNumber[i]--;
-		sign[location] = i;
-
-		// 모든 부호를 썻다면 계산하고 max와 min과 비교
-		if (location == N)
+		for (int o = 1; o <= N; o++)
 		{
-			int calculationNumber = Calculation(); // 계산식을 계산한 값
-			max = std::max(max, calculationNumber);
-			min = std::min(min, calculationNumber);
+			std::cin >> S[i][o];
 		}
-		// 쓰지 않은 부호가 남아있다면 다음 location으로 진행
-		else
-			EnterSign(location + 1);
-			
-		// 돌아왔다면 넣었던 부호를 빼고 다시 진행
-		signNumber[i]++;
-		sign[location] = 0;
-		
+		ateam[i] = false; // 1팀 초기화
 	}
+
+	min = 2147483647; // 최솟값 초기화
+
+	// AddPerson을 호출한다. nextPerson을 1에서 2/N + 1까지만 넣는다.
+	for (int i = 1; i <= N / 2 + 1; i++)
+	{
+		AddPerson(i, 1);
+	}
+
+	// min을 출력한다.
+	std::cout << min;
 }
 
-int Calculation() // 부호를 모두 채워넣었을 때 계산해줄 함수
+/// <summary>
+/// 1팀에 2/N만큼의 선수를 백트래킹으로 채워주는 함수
+/// </summary>
+/// <param name="nextPerson">1팀에 넣을 다음 선수</param>
+/// <param name="size">1팀에 들어간 선수들의 수</param>
+void AddPerson(int nextPerson, int size)
 {
-	int answer = 0;
+	// nextPerson을 1팀에 넣는다.
+	ateam[nextPerson] = true;
 
-	for (int i = 1; i <= N; i++)
+	// size가 2/N과 같다면 CalculateStats()를 호출하여 min과 비교해 더 작은 값은 min에 저장하고 nextPerson을 1팀에서 뺀후 함수를 종료한다.
+	if (size == N / 2)
 	{
-		switch (sign[i])
+		min = std::min(min, CalculateStats());
+		ateam[nextPerson] = false;
+		return;
+	}
+
+	// size가 2/N보다 작다면 AddPerson을 다시 한번 호출한다. nextPerson은 현재 값보다 큰 숫자부터 넣는다. size는 +1한다.
+	// size가 2/N이 안됐는데 nextPerson이 N과 같다면 nextPerson을 1팀에서 빼고 함수를 종료한다.
+	if (nextPerson >= N)
+	{
+		ateam[nextPerson] = false;
+		return;
+	}
+	else if (size < N /2)
+	{
+		for (int i = nextPerson + 1; i <= N; i++)
 		{
-			case 1:
-				answer += number[i];
-				break;
-			case 2:
-				answer -= number[i];
-				break;
-			case 3:
-				answer *= number[i];
-				break;
-			case 4:
-				answer /= number[i];
-				break;
+			AddPerson(i, size + 1);
 		}
 	}
 
-	return answer;
+	// nextPerson을 1팀에서 빼고 함수를 종료한다.
+	ateam[nextPerson] = false;
+	return;
+}
+
+int CalculateStats()
+{
+	int ateamStats = 0; // 1팀 능력치
+	int bteamStats = 0; // 2팀 능력치
+
+	// 1팀 능력치 계산
+	// 2팀 능력치 계산
+	for (int i = 1; i < N; i++)
+	{
+		if (ateam[i]) // 1팀 능력치 계산
+		{
+			for (int o = i + 1; o <= N; o++)
+			{
+				if (ateam[o])
+				{
+					ateamStats += S[i][o];
+					ateamStats += S[o][i];
+				}
+			}
+		}
+		else // 2팀 능력치 계산
+		{
+			for (int o = i + 1; o <= N; o++)
+			{
+				if (!ateam[o])
+				{
+					bteamStats += S[i][o];
+					bteamStats += S[o][i];
+				}
+			}
+		}
+	}
+
+	// 1팀 능력치 - 2팀 능력치
+	// 양수면 반환, 음수면 -1 곱해서 반환
+	if (ateamStats - bteamStats >= 0)
+	{
+		return ateamStats - bteamStats;
+	}
+	else
+	{
+		return bteamStats - ateamStats;
+	}
 }

1cc26a7222535406d7e171e0e7403c21d7a62173
jmk1118 <jmk1118@naver.com>
2021-09-10 16:29:02 +0900

    Create Q11053.cpp

---
+++ Baekjoon Complete Code/Q11053.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+
+int main(void)
+{
+	int N;
+	int number;
+	std::vector<int> A;
+	
+	std::cin >> N;
+	std::cin >> number;
+	A.push_back(number);
+
+	for (int i = 2; i <= N; i++)
+	{
+		std::cin >> number;
+		if (number > A.back())
+			A.push_back(number);
+		else
+		{
+			for (int o = 0; o < A.size(); o++)
+			{
+				if (A.at(o) >= number)
+				{
+					A.at(o) = number;
+					break;
+				}
+			}
+		}
+	}
+
+	std::cout << A.size();
+}

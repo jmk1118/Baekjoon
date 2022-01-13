d34e989e51fd7714ad138300ad3c6b7ee3ba2e0e
jmk1118 <jmk1118@naver.com>
2022-01-13 15:07:57 +0900

    Create Q05086.cs

---
+++ Q05086.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split(" ");
+        int front = Int32.Parse(input[0]);
+        int back = Int32.Parse(input[1]);
+        
+        while (front != 0 || back != 0)
+        {
+            if (back % front == 0)
+            {
+                Console.WriteLine("factor");
+            }
+            else if (front % back == 0)
+            {
+                Console.WriteLine("multiple");
+            }
+            else
+            {
+                Console.WriteLine("neither");
+            }
+
+            input = Console.ReadLine().Split(" ");
+            front = Int32.Parse(input[0]);
+            back = Int32.Parse(input[1]);
+        }
+    }
+}

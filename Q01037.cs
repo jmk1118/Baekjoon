8177090ead19957c5cd4d9166254a908149ba20f
jmk1118 <jmk1118@naver.com>
2022-01-14 15:46:54 +0900

    Create Q01037.cs

---
+++ Q01037.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split(" ");
+        int N = Int32.Parse(input[0]);
+
+        input = Console.ReadLine().Split(" ");
+        int[] divisors = new int[input.Length];
+        for (int i = 0; i < input.Length; i++)
+        {
+            divisors[i] = Int32.Parse(input[i]);
+        }
+
+        Array.Sort(divisors);
+
+        Console.WriteLine(divisors[0] * divisors[divisors.Length - 1]);
+    }
+}

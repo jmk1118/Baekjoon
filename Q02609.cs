4097d76899aa69a89f23534850a12eb667a831c7
jmk1118 <jmk1118@naver.com>
2022-01-17 16:18:18 +0900

    Create Q02609.cs

---
+++ Q02609.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split(" ");
+        int A = Int32.Parse(input[0]);
+        int B = Int32.Parse(input[1]);
+        if (A > B)
+        {
+            A = B;
+            B = Int32.Parse(input[0]);
+        }
+
+        int answerone = 0;
+        int answertwo = 0;
+
+        for (int i = 1; i <= B; i++)
+        {
+            if (i <= A)
+            {
+                if(A % i == 0 && B % i == 0)
+                {
+                    answerone = i;
+                }
+            }
+            if ((A * i) % B == 0 && answertwo == 0)
+            {
+                answertwo = A * i;
+            }
+        }
+
+        Console.WriteLine(answerone);
+        Console.WriteLine(answertwo);
+    }
+}

19c5a87d6aed4215826d7c2f87468076294979ea
jmk1118 <jmk1118@naver.com>
2022-01-31 10:45:48 +0900

    Create Q11050.cs

---
+++ Q11050.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split(" ");
+        int n = Int32.Parse(input[0]);
+        int k = Int32.Parse(input[1]);
+
+        int answer = Factorial(n) / (Factorial(k) * Factorial(n - k));
+
+        Console.WriteLine(answer);
+    }
+
+    static int Factorial(int x)
+    {
+        int answer = 1;
+
+        for(int i = 1; i <= x; i++)
+        {
+            answer *= i;
+        }
+
+        return answer;
+    }
+}

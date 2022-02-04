80564658c012587c6acbe58547d1c08fa51edd64
jmk1118 <jmk1118@naver.com>
2022-02-04 15:41:18 +0900

    Create Q18108.cs

---
+++ Q18108.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        Console.WriteLine(Int32.Parse(input) - 543);
+    }
+}

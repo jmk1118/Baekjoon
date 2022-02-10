aee2e82322af3aff8a74296e30ed05ab7bfbaaec
jmk1118 <jmk1118@naver.com>
2022-02-10 12:29:13 +0900

    Create Q02004.cs

---
+++ Q02004.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split(" ");
+        long N = Int32.Parse(input[0]);
+        long M = Int32.Parse(input[1]);
+
+        long two = HowManyTwo(N) - HowManyTwo(M) - HowManyTwo(N - M);
+        long five = HowManyFive(N) - HowManyFive(M) - HowManyFive(N - M);
+
+        Console.WriteLine(Math.Min(two, five));
+    }
+
+    static long HowManyTwo(long x)
+    {
+        long answer = 0;
+        while(x >= 2)
+        {
+            answer += x / 2;
+            x /= 2;
+        }
+        return answer;
+    }
+    
+    static long HowManyFive(long x)
+    {
+        long answer = 0;
+        while(x >= 5)
+        {
+            answer += x / 5;
+            x /= 5;
+        }
+        return answer;
+    }
+}

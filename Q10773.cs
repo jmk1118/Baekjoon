a43053a21483ace5a07d53adbb5c56b8a5be8851
jmk1118 <jmk1118@naver.com>
2022-02-11 11:21:15 +0900

    Create Q10773.cs

---
+++ Q10773.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split(" ");
+        int N = Int32.Parse(input[0]);
+        Stack<int> stack = new Stack<int>();
+
+        int x;
+        for(int i = 0; i < N; i++)
+        {
+            input = Console.ReadLine().Split(" ");
+            x = Int32.Parse(input[0]);
+
+            if(x == 0)
+            {
+                stack.Pop();
+            }
+            else
+            {
+                stack.Push(x);
+            }
+        }
+
+        long answer = 0;
+        while(stack.Count > 0)
+        {
+            answer += stack.Pop();
+        }
+        Console.WriteLine(answer);
+    }
+}

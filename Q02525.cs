cbbc1c8f46a9702e3c6a108f4ba73bb1554440b9
jmk1118 <jmk1118@naver.com>
2022-02-15 23:00:16 +0900

    Create Q02525.cs

---
+++ Q02525.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split(" ");
+        int A = Int32.Parse(input[0]);
+        int B = Int32.Parse(input[1]);
+        input = Console.ReadLine().Split(" ");
+        int C = Int32.Parse(input[0]);
+
+        int answer = A * 60 + B + C;
+        if(answer / (24 * 60) > 0)
+        {
+            answer = answer % (24 * 60);
+        }
+        Console.WriteLine(answer / 60 + " " + answer % 60);
+    }
+}
